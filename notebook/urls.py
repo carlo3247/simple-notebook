af41ff009d06a8f7a6b80a15582df1fabcde928c
carlo3247 <cawinkelhake@aol.com>
2017-10-31 16:57:30 +0000

    jsdfgjongs
    sdfgdfgsdsfg

--- notebook/urls.py
+++ notebook/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
 
     url(r'^$', login_required(views.NoteView.as_view()), name='home'),
     # url(r'^$', views.NoteView.as_view(), name='home'),
-    url(r'^(?P<pk>[0-9]+)/detail/$', views.DetailedNote.as_view(), name='detail'),
-    url(r'^add/$', views.AddNote.as_view(), name='add'),
-    url(r'^(?P<pk>[0-9]+)/edit/$', views.EditNote.as_view(), name='edit'),
-    url(r'^(?P<pk>[0-9]+)/delete/$', views.DeleteNote.as_view(), name='delete'),
+    url(r'^(?P<pk>[0-9]+)/detail/$', login_required(views.DetailedNote.as_view()), name='detail'),
+    url(r'^add/$', login_required(views.AddNote.as_view()), name='add'),
+    url(r'^(?P<pk>[0-9]+)/edit/$', login_required(views.EditNote.as_view()), name='edit'),
+    url(r'^(?P<pk>[0-9]+)/delete/$', login_required(views.DeleteNote.as_view()), name='delete'),
 ]
