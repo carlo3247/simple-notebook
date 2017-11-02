a416d242b1c4b4deaf923d71808a2aec2fee5020
carlo3247 <cawinkelhake@aol.com>
2017-11-02 14:14:31 +0000

    hm
    hm

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+NUM_WORKERS=3
+TIMEOUT=120
+
 # Start Gunicorn processes
 echo Starting Gunicorn.
 exec gunicorn team12scenario.wsgi:application \
+    --workers $NUM_WORKERS \
+    --timeout $TIMEOUT \
     --bind 0.0.0.0:8000 \
-    --workers 3
