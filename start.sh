a5955ef5b87bfb8f05644e6a52824edc7e247f39
carlo3247 <cawinkelhake@aol.com>
2017-11-02 16:43:47 +0000

    ok
    ok

--- start.sh
+++ start.sh
@@ -3,9 +3,15 @@
 NUM_WORKERS=3
 TIMEOUT=120
 
-# Start Gunicorn process
+# Start Gunicorn processes
 echo Starting Gunicorn.
 exec gunicorn team12scenario.wsgi:application \
     --workers $NUM_WORKERS \
     --timeout $TIMEOUT \
     --bind 0.0.0.0:8000 \
+
+
+
+    docker ps -q -a | xargs docker rm
+    docker build -t djangoapp:$BUILD_NUMBER .
+    docker run -p 8000:8000 djangoapp:$BUILD_NUMBER &
